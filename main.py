f806d972926008b9899bbf3dea7f174f3e02ce5a
Francimar Alexandre <92753716+AlexandreDNTS@users.noreply.github.com>
2022-12-08 18:55:30 -0300

    cor de texto e butões

--- main.py
+++ main.py
@@ -1,55 +1,55 @@
 import PySimpleGUI as sg
 
-usuario = [' ']
-senha = [' ']
-
+usuario = ['francimar']
+senha = ['1']
+usuarioLOGADO = []
 
 def tela_inicial():
     layout = [
-        [sg.Text('\n\n\t\t\t\tAGENDA\t\t')],
+        [sg.Text('\n\n\t\t\t\tAGENDA\t\t',text_color='black')],
         [sg.Text('\n\n')],
-        [sg.Button('login',size=(15,2))], 
-        [sg.Button('cadastro',size=(15,2))]
+        [sg.Button('login',size=(15,2),button_color='blue')], 
+        [sg.Button('cadastro',size=(15,2),button_color='blue')]
         ]
     return sg.Window('AGENDA', layout=layout, finalize=True, size=(600, 400))
 
 
 def tela_login():
     layout = [
-        [sg.Text('\n\n\t\tAGENDA - LOGIN\t\t')],
+        [sg.Text('\n\n\t\tAGENDA - LOGIN\t\t',text_color='black')],
         [sg.Text('\n\n')],
-        [sg.Text('usuário')],
+        [sg.Text('usuário',text_color='black')],
         [sg.Input(key='usuarioL')],
-        [sg.Text('senha')],
+        [sg.Text('senha',text_color='black')],
         [sg.Input(key='senhaL', password_char='*')],
-        [sg.Text('', key='msgL')],
-        [sg.Button('LOGIN',size=(15,2)), sg.Button('CADASTRAR',size=(15,2))],
-        [sg.Button('voltar',size=(15,2))]
+        [sg.Text('', key='msgL',text_color='black')],
+        [sg.Button('LOGIN',size=(15,2),button_color='blue'), sg.Button('CADASTRAR',size=(15,2),button_color='blue')],
+        [sg.Button('voltar',size=(15,2),button_color='blue')]
     ]
     return sg.Window('AGENDA-LOGIN', layout=layout, finalize=True, size=(600, 400))
 
 
 def tela_cadastro():
     layout = [
-        [sg.Text('\n\n\t\tAGENDA - CADASTRO\t\t')],
+        [sg.Text('\n\n\t\tAGENDA - CADASTRO\t\t',text_color='black')],
         [sg.Text('\n\n')],
-        [sg.Text('usuário')],
+        [sg.Text('usuário',text_color='black')],
         [sg.Input(key='usuarioC')],
-        [sg.Text('senha')],
+        [sg.Text('senha',text_color='black')],
         [sg.Input(key='senhaC', password_char='*')],
-        [sg.Text('confirmar senha')],
+        [sg.Text('confirmar senha',text_color='black')],
         [sg.Input(key='Consenha', password_char='*')],
-        [sg.Text('', key='msgC')],
-        [sg.Button('CADASTRAR',size=(15,2))]
+        [sg.Text('', key='msgC',text_color='black')],
+        [sg.Button('CADASTRAR',size=(15,2),button_color='blue')]
     ]
     return sg.Window('AGENDA-CADASTRO', layout=layout, finalize=True, size=(600, 400))
 
 
 def tela_usuario():
     layout = [
-        [sg.Text('\n\n\t\tAGENDA - USUÁRIO\t\t')],
+        [sg.Text('\n\n\t\tAGENDA - USUÁRIO\t\t',text_color='black')],
         [sg.Text('\n\n')],
-        [sg.Text('', key='nomeUsuario')],
+        [sg.Text('', key='msgUsuario',text_color='black')]
         ]
     return sg.Window('AGENDA-USUÁRIO', layout=layout, finalize=True, size=(600, 400))
 
@@ -75,9 +75,10 @@ while True:
     if window == TelaLogin and eventos == 'CADASTRAR':
         telaCadastro = tela_cadastro()
         TelaLogin.hide()
+    
     if window == TelaLogin and eventos == 'LOGIN':
 
-        if valores['usuarioL'] == '' or valores['senhaL'] == '':
+        if valores['usuarioL'] == '' or valores['senhaL'] == '' or valores['usuarioL']==' ' or valores['senhaL'] == ' ':
             window['msgL'].update('todos os campos devem ser preenchidos')
         else:
             for i in range(0, len(usuario)):
